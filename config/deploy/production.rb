3806893cd74f02e8458f4f2227fc3758bef2568b
Geoffrey Roberts <g.roberts@blackicemedia.com>
2013-03-08 22:46:30 +1100

    Trying to separate out stages

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,3 +2,8 @@ server 'linode.smash.org.au', :app, :web, :primary => true
 set :deploy_to, '/var/www/smash.org.au/vendors'
 set :db_name,     "vendors_production"
 set :url,         "http://vendors.smash.org.au"
+
+set(:releases_path)     { File.join(deploy_to, version_dir) }
+set(:shared_path)       { File.join(deploy_to, shared_dir) }
+set(:current_path)      { File.join(deploy_to, current_dir) }
+set(:release_path)      { File.join(releases_path, release_name) }
